a1c856fce4a8f4a260572c9e533675d33bb25393
vsshk <v.s.shkodin@gmail.com>
2021-04-10 21:54:01 -0700

    added addoptios -H headless

--- conftest.py
+++ conftest.py
@@ -8,6 +8,7 @@ import chromedriver_binary  # Adds chromedriver binary to path
 def pytest_addoption(parser):
     parser.addoption("-H",action="store_true", default=False)
     parser.addoption("-I", action="store_true", default=False)
+    parser.addoption("-L", action="store_true", default=False)
     parser.addoption('--base_url', action='store')
 
 
@@ -47,8 +48,13 @@ def pytest_sessionfinish(session, exitstatus):
 
 
 @pytest.fixture()
-def driver_init(request):
-    driver = webdriver.Chrome()
+def driver_init(request, headless):
+    if headless:
+        options = Options()
+        options.headless = True
+        driver = webdriver.Chrome(options=options)
+    else:
+        driver = webdriver.Chrome()
     driver.get(request.config.getoption("--base_url"))
     driver.implicitly_wait(15)
     driver.maximize_window()
@@ -62,8 +68,8 @@ def driver_init(request):
 
 
 @pytest.fixture()
-def H(request):
-    return request.config.getoption("-H")
+def HEALING(request):
+    return request.config.getoption("-L")
 
 @pytest.fixture()
 def init_run(request):
@@ -72,3 +78,6 @@ def init_run(request):
 @pytest.fixture()
 def base_url(request):
     return request.config.getoption("--base_url")
+@pytest.fixture()
+def headless(request):
+    return request.config.getoption("-H")
