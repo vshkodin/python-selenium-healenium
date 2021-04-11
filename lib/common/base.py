2119dde1c8e475711d4ec1c39f6b0b09e2adbfd4
vsshk <v.s.shkodin@gmail.com>
2021-04-11 10:14:57 -0700

    code refactor

--- lib/common/base.py
+++ lib/common/base.py
@@ -1,3 +1,9 @@
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+
 import os
 import codecs
 
@@ -5,12 +11,17 @@ class Driver:
     def __init__(self,driver):
         self.driver=driver
 
+    def find(self,LOCATOR):
+        WebDriverWait(self.driver, 30).until(EC.presence_of_element_located((By.CSS_SELECTOR, LOCATOR)))
+        elem = self.driver.find_element_by_css_selector(LOCATOR)
+        return elem
+
     def get(self, URL):
-        self.driver.get(URL)
+        self.driver.get(f'{self.driver.base_url}{URL}')
         self.page_saver()
 
     def click(self,LOCATOR):
-        elem = self.driver.find_element_by_css_selector(LOCATOR)
+        elem = self.find(LOCATOR)
         elem.click()
 
     def page_source(self):
@@ -24,7 +35,7 @@ class Driver:
 
     def page_saver(self):
         url = self.get_url()
-        url = url.replace(self.driver.base_url,'').replace(r'/','_')
+        url = url.replace(self.driver.base_url,'').replace(r'/','_').replace(r'?','_').replace(r'=','_').replace(r'.','_')
         n = os.path.join("pages\\", f"{url}.html")
         f = codecs.open(n, "w", "utf−8")
         h = self.page_source()
--- test_automationpractice.py
+++ test_automationpractice.py
@@ -1,12 +1,9 @@
 import pytest
 
 
-def test_one(init_run, driver_init):
+def test_automationpractice(init_run, driver_init):
     if init_run:
         driver_init.click('a[title="Women"]')
-        #print(driver_init.page_source())
         assert "Women - My Store" in driver_init.title()
-        driver_init.get("http://automationpractice.com/index.php")
-        print("RECORD HTML THREE DUE RUN")
-        print("IF TITLE OF THE PAGE DIFF CREATE NEW FILE")
-    assert 1
+        driver_init.get("/index.php")
+        driver_init.get("/index.php?controller=contact")
